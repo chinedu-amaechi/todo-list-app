a75f57f294da6bcc69b7ae56f4e6ec280a878acb
Chinedu Amaechi <chymez56@yahoo.com>
2024-10-24 16:11:13 -0600

    TodoList
    creating list of ToDo

---
+++ ToDoForm.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function ToDoForm() {
+    return (
+        <View style={styles.form}>
+            <TextInput
+                style={styles.input}
+                placeholder="Add a new task..."
+            />
+            <Button title="Add" />
+        </View>
+    );
+}
